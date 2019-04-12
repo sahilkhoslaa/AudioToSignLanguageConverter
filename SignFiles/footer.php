93992ae5ee63c3fac95c6d6c2d00d320126e632f
RahulGarg <garg.rahul1997@gmail.com>
2019-04-12 15:17:45 +0530

    directory restructure

--- SignFiles/footer.php
+++
@@ -1,7 +0,0 @@
-<div class="container">
-    <div class="row">
-        <div class="col-md-12 text-center" style="font-size:13px; margin-bottom:20px;"><hr>
-"EXPLORE INNOVATE EXCEL" - PEC University of Technology
-        </div>
-    </div>
-</div>
